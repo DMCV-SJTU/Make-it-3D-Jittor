35fafd11d20bb8393ffe47f77afcfd8dc961cadb
DMCV-SJTU <hu-teng@sjtu.edu.cn>
2024-06-27 15:07:54 +0800

    Update preprocess.py

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -4,17 +4,18 @@ import os
 import shutil
 import traceback
 
-def move_file(src_path, dst_path, file):
+def move_file(src_path, dst_path):
     print 'from : ',src_path
     print 'to : ',dst_path
     try:
         # cmd = 'chmod -R +x ' + src_path
         # os.popen(cmd)
-        f_src = os.path.join(src_path, file)
-        if not os.path.exists(dst_path):
-            os.mkdir(dst_path)
-        f_dst = os.path.join(dst_path, file)
-        shutil.move(f_src, f_dst)
+        for file in os.listdir(src_path):
+            f_src = os.path.join(src_path, file)
+            if not os.path.exists(dst_path):
+                os.mkdir(dst_path)
+            f_dst = os.path.join(dst_path, file)
+            shutil.move(f_src, f_dst)
     except Exception as e:
         print 'move_file ERROR: ',e
         traceback.print_exc()
