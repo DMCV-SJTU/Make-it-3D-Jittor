d62efaee199083f31e598a913107cf784701f9af
DMCV-SJTU <hu-teng@sjtu.edu.cn>
2024-06-28 21:24:31 +0800

    Update mv2workspace.py

--- preprocess/mv2workspace.py
+++ preprocess/mv2workspace.py
@@ -1,28 +1,28 @@
-#coding=utf-8
+import PIL.Image as Image
+import numpy as np
 import argparse
 import os
-import shutil
-import traceback
 
-def move_file(src_path, dst_path):
-    print 'from : ',src_path
-    print 'to : ',dst_path
-    try:
-        # cmd = 'chmod -R +x ' + src_path
-        # os.popen(cmd)
-        for file in os.listdir(src_path):
-            f_src = os.path.join(src_path, file)
-            if not os.path.exists(dst_path):
-                os.mkdir(dst_path)
-            f_dst = os.path.join(dst_path, file)
-            shutil.move(f_src, f_dst)
-    except Exception as e:
-        print 'move_file ERROR: ',e
-        traceback.print_exc()
+
+def convert_rgb2rgba(input_path, mask_path, output_path):
+    img = Image.open(input_path).convert('RGBA')
+    mask = Image.open(mask_path).convert('L')
+    img = np.array(img)
+    mask = np.array(mask)
+    img[:, :, 3] = mask
+    img = Image.fromarray(img).convert('RGBA')
+    img.save(output_path)
+
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  opt = parser.parse_args()
-  parser.add_argument('--workspace', type=str, default='workspace')
-  opt.workspace = os.path.join('../results', opt.workspace)
-  move_files('./preprocess', opt.workspace )
+    parser = argparse.ArgumentParser()
+    opt = parser.parse_args()
+    parser.add_argument('--input', type=str, default=None)
+    parser.add_argument('--mask', type=str, default=None)
+    parser.add_argument('--output', type=str, default='preprocess')
+    if opt.input  is None or opt.mask is None:
+        print("there is no image or mask")
+        sys.exit(0)
+    filename = os.path.basename(opt.input)
+    save_path = os.path.join(opt.output, filename)
+    convert_rgb2rgba(opt.input, opt.mask, save_path)
